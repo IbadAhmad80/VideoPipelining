38b4607687d2c6d254282db5b466ccca93ac4b81
IbadAhmad80 <ibad23ahmad@gmail.com>
2023-03-11 17:50:20 +0500

    Remove large videos

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const routes = require("./routes");
+const res = require("express/lib/response");
 
 app.use(cors());
 
 routes(app);
 
 app.listen(8000);
+
+let a = "Hello World";
+
+a = "Not Hellp World";
+
+app.get("/", (_, res) => {
+    res.send("perfect");
+});
